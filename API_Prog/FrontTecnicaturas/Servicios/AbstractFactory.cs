91ea3fa8a87978bd94d8bd097b3bdd940d98096f
TomiBenza <111018952+TomiBenza@users.noreply.github.com>
2022-11-03 02:54:27 -0300

    Add files via upload

---
+++ API_Prog/FrontTecnicaturas/Servicios/AbstractFactory.cs
@@ -0,0 +1,16 @@
+﻿using FrontTecnicaturas.Servicios.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FrontTecnicaturas.Servicios
+{
+    abstract class AbstractFactory
+    {
+        public abstract IServicio CrearServicio();
+
+    }
+}
---
+++ API_Prog/FrontTecnicaturas/Servicios/Factory.cs
@@ -0,0 +1,19 @@
+﻿using FrontTecnicaturas.Servicios.Implementaciones;
+using FrontTecnicaturas.Servicios.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FrontTecnicaturas.Servicios
+{
+    internal class Factory : AbstractFactory
+    {
+        public override IServicio CrearServicio()
+        {
+            return new Servicio();
+        }
+    }
+}
