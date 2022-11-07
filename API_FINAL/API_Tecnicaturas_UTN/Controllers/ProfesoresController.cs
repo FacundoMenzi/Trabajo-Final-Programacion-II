c935192f0bc10a55bbbcb8b69c0a13c552abb8b1
TomiBenza <111018952+TomiBenza@users.noreply.github.com>
2022-11-06 22:26:22 -0300

    Add files via upload

---
+++ API_FINAL/API_Tecnicaturas_UTN/Controllers/ProfesoresController.cs
@@ -0,0 +1,31 @@
+﻿using DLL_API.Dominio;
+using DLL_API.Fachada;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Tecnicaturas_UTN.Controllers
+{
+    public class ProfesoresController : Controller
+    {
+        private IDataAPI dataApi; 
+
+        public ProfesoresController()
+        {
+            dataApi = new DataAPI_Imp();
+        }
+        [HttpGet("/profesores/ver")]
+        public IActionResult GetProfesores()
+        {
+            List<Profesor> lst;
+            try
+            {
+                lst = dataApi.GetProfesores();
+                return Ok(lst);
+
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Error interno! Intente luego");
+            }
+        }
+    }
+}
